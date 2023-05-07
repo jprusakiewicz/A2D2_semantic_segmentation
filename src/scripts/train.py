8abba60bfd0bbe9eed791a81dda8241b6eba9c6e
jakub.prusakiewicz <jakub.prusakiewicz@silenteight.com>
2023-05-07 11:28:16 +0200

    cleanup

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -4,7 +4,7 @@ from PIL import ImageColor
 import sys
 
 sys.path.append("src")
-from read_data import SAMPLE_IMAGE, SAMPLE_LABEL
+from data.read_data import SAMPLE_IMAGE, SAMPLE_LABEL
 from models.unet.test_unet import unet_model
 from preprocessors.label import label_to_categorical
 
@@ -26,3 +26,13 @@ def train_model():
     cat = label_to_categorical(SAMPLE_LABEL, class_colors_mapping_rgb)
     my_model.fit(x=np.expand_dims(SAMPLE_IMAGE, axis=0), y=cat, epochs=15, steps_per_epoch=1, verbose=2)
     return my_model
+
+
+def save_model(model, path: str = "./"):
+    model.save(path)
+
+
+if __name__ == '__main__':
+    MODEL_SAVE_PATH = "./trained_models/keras_test_unet"
+    model = train_model()
+    save_model(model, MODEL_SAVE_PATH)
